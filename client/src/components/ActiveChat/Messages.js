4da55231d66039c9d924099cc8647032bbfc8ce5
James <jamesmca@live.com>
2021-09-02 11:10:52 -0700

    Update Messages.js
    Just removed 1 line

--- client/src/components/ActiveChat/Messages.js
+++ client/src/components/ActiveChat/Messages.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Box } from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
 import { SenderBubble, OtherUserBubble } from "../ActiveChat";
 import moment from "moment";
 
